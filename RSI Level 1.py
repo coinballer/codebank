d136d666e1a69dab08791a9b084c5cfbe7738538
Coinballer <104280192+coinballer@users.noreply.github.com>
2022-04-26 21:37:36 -0600

    Create RSI Level 1.py

---
+++ RSI Level 1.py
@@ -0,0 +1,24 @@
+#Top Skill 1. Python Coding
+#Import Python Libraries
+import requests
+from IPython.display import Image, display
+
+#Top Skill 2. Get API Data
+getApiData = requests.get('https://coinballer.io/api/technical-indicators-daily/APIKEY')
+ApiData = getApiData.json()
+
+#Top Skill 3. Variables
+ApiDataToday = ApiData[-1]
+RSI = ApiDataToday['rsi']
+BTCPrice = ApiDataToday['price_close']
+
+#Top Skill 4. If Statements
+if RSI > 50:
+    Scoreboard = 'https://i.imgur.com/wjbQ2dq.png'
+else: Scoreboard = 'https://i.imgur.com/C88GgnO.png'
+
+print('BTC Price')
+print(BTCPrice)
+display(Image(Scoreboard))
+print('RSI')
+print(RSI)
