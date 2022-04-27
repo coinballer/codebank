c9aab13a6c22b74762f0c06c7d00b434c8da02c6
Coinballer <104280192+coinballer@users.noreply.github.com>
2022-04-26 21:42:53 -0600

    Create BOLL Level 1.py

---
+++ BOLL Level 1.py
@@ -0,0 +1,24 @@
+#Top Skill 1. Python Coding
+#Import Python Libraries
+import requests
+from IPython.display import Image, display
+
+#Top Skill 2. Get API Data
+getApiData = requests.get('https://coinballer.io/api/technical-indicators-daily/APIKEY')
+ApiData = getApiData.json()
+
+#Top Skill 3. Variables
+ApiDataToday = ApiData[-1]
+BOLL = ApiDataToday['bollinger_middle']
+BTCPrice = ApiDataToday['price_close']
+
+#Top Skill 4. If Statements
+if BTCPrice > BollingerMiddle:
+    Scoreboard = 'https://i.imgur.com/wjbQ2dq.png'
+else: Scoreboard = 'https://i.imgur.com/C88GgnO.png'
+
+print('BTC Price')
+print(BTCPrice)
+display(Image(Scoreboard))
+print('BOLL')
+print(BOLL)
