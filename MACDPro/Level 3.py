d08ce1197e1187c12ca8d2c7c3f9d847ece2afec
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-14 11:20:38 -0600

    Create Level 3.py

---
+++ MACDPro/Level 3.py
@@ -0,0 +1,40 @@
+#TOP SKILL #1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
+!pip install plotly
+import requests
+import plotly.express
+import pandas
+
+#TOP SKILL #2: IMPORTING API DATA
+getApiData = requests.get("https://coinballer.io/api/macd-pro/APIKEY")
+ApiData = getApiData.json()
+
+#TOP SKILL #3: DECLARE AND SET VARIABLES
+#Most Recent 150 Days (Slice Data)
+NumberofDays = len(ApiData)
+ApiData = ApiData[NumberofDays - 150:NumberofDays]
+ApiDataFrame = pandas.DataFrame(ApiData)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartPrice = plotly.express.bar(x=ApiDataFrame.date, y=ApiDataFrame.price_close)
+chartPrice.update_traces(marker_color=ApiDataFrame.macd_pro_color)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartPoints = plotly.express.area(x=ApiDataFrame.date, y=ApiDataFrame.daily_points)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartWeekly = plotly.express.line(x=ApiDataFrame.date, y=ApiDataFrame.daily_macd_line)
+chartWeekly.update_traces(line=dict(color="black", width=5))
+chartWeekly.add_trace(go.Scatter(x=ApiDataFrame.date, y=ApiDataFrame.daily_signal_line, fill='tozeroy', line=dict(color="#eeaf12", width=5), name="signal"))
+chartWeekly.add_trace(go.Bar(x=ApiDataFrame.date, y=ApiDataFrame.daily_histogram, marker_color="DeepSkyBlue", name="histogram"))
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartDaily = plotly.express.line(x=ApiDataFrame.date, y=ApiDataFrame.daily_macd_line)
+chartDaily.update_traces(line=dict(color="black", width=5))
+chartDaily.add_trace(go.Scatter(x=ApiDataFrame.date, y=ApiDataFrame.daily_signal_line, fill='tozeroy', line=dict(color="#eeaf12", width=5), name="signal"))
+chartDaily.add_trace(go.Bar(x=ApiDataFrame.date, y=ApiDataFrame.daily_histogram, marker_color="DeepSkyBlue", name="histogram"))
+
+#SHOW SCOREBOARD
+chartPrice.show()
+chartPoints.show()
+chartWeekly.show()
+chartDaily.show()
