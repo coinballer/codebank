f246c690a2be153e200c7cab731745b4ad2283d4
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-14 08:12:12 -0600

    Create Level 3.py

---
+++ WLPR/Level 3.py
@@ -0,0 +1,44 @@
+#TOP SKILL #1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
+!pip install plotly
+import requests
+import plotly.express
+import pandas
+
+#TOP SKILL #2: IMPORTING API DATA
+getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+ApiData = getApiData.json()
+
+#TOP SKILL #3: DECLARE AND SET VARIABLES
+#Most Recent 150 Days (Slice Data)
+NumberofDays = len(ApiData)
+ApiData = ApiData[NumberofDays - 150:NumberofDays]
+ApiDataFrame = pandas.DataFrame(ApiData)
+
+#TOP SKILL #3: DECLARING VARIABLES (EMPTY BOX)
+WLPRColor = []
+WLPRPoints = []
+
+#TOP SKILL #2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
+for i in ApiData:
+    if i['wlpr'] > -50:
+     iWLPRColor = 'green'
+    else: 
+     iWLPRColor = 'red'
+     WLPRColor.append(iWLPRColor)
+    iWLPRPoints = 100 + i['wlpr']
+    WLPRPoints.append(iWLPRPoints)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartPrice = plotly.express.bar(x=ApiDataFrame.date, y=ApiDataFrame.price_close)
+chartPrice.update_traces(marker_color=WLPRColor)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartPoints = plotly.express.area(x=ApiDataFrame.date, y=WLPRPoints)
+
+#TOP SKILL: CREATE A CHART USING PLOTLY
+chartDaily = plotly.express.line(x=ApiDataFrame.date, y=ApiDataFrame.wlpr)
+
+#SHOW SCOREBOARD
+chartPrice.show()
+chartPoints.show()
+chartDaily.show()
