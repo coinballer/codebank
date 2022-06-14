448e52b9666c38d782ba1554659d664364f0ed50
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-14 08:04:16 -0600

    Update Level 1.py

--- MACD/Level 1.py
+++ MACD/Level 1.py
@@ -3,32 +3,19 @@ import requests
 from IPython.display import Image, display
 
 #TOP SKILL #2: GET API DATA
-getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+getApiData = requests.get("https://coinballer.io/api/macd-pro/APIKEY")
 ApiData = getApiData.json()
 
 #TOP SKILL 3: DECLARE AND SET VARIABLES
 #Slice ApiData For Most Recent Date (Today)
 ApiData_Today = ApiData[-1]
-BollingerUpper = ApiData_Today['bollinger_upper']
-BollingerMiddle = ApiData_Today['bollinger_middle']
-BollingerLower = ApiData_Today['bollinger_lower']
-BitcoinPrice = ApiData_Today['price_close']
-BollingerSpread = BollingerUpper - BollingerLower
-BollingerPriceSpread = BitcoinPrice - BollingerLower
-
-#TOP SKILL 4: SIMPLE IF STATEMENT
-if BitcoinPrice > BollingerUpper:
-    BollingerPoints = 100
-else: 
-  if BitcoinPrice < BollingerLower:
-    BollingerPoints = 0
-  else: BollingerPoints = round(BollingerPriceSpread / BollingerSpread, 1) * 100
+MACDPoints = ApiData_Today['daily_points']
 
 #TOP SKILL #4: SIMPLE IF STATEMENT
-if BitcoinPrice > BollingerMiddle:
+if MACDPoints > 50:
     Arrow = 'https://i.imgur.com/aCTsoA0.png'
 else: Arrow = 'https://i.imgur.com/8Li7d4A.png'
 
 #SHOW SCOREBOARD
-print("BOLL", BollingerPoints)
+print("MACD", MACDPoints)
 display(Image(Arrow, width = 150, height = 150))
