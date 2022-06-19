72164d9259b6a448c6e0658977bb4b16c89a80e8
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-19 13:04:27 -0600

    Update LEVEL 3 - Charts.py

--- WLPR/LEVEL 3 - Charts.py
+++ WLPR/LEVEL 3 - Charts.py
@@ -1,24 +1,24 @@
-#TOP SKILL #1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
+#TOP SKILL 1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
 !pip install plotly
 import requests
 import plotly.express
 import pandas
 
-#TOP SKILL #2: IMPORTING API DATA
-getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+#TOP SKILL 2: IMPORTING API DATA
+getApiData = requests.get("https://coinballer.io/api/pro-daily/APIKEY")
 ApiData = getApiData.json()
 
-#TOP SKILL #3: DECLARE AND SET VARIABLES
+#TOP SKILL 3: DECLARE AND SET VARIABLES
 #Most Recent 150 Days (Slice Data)
 NumberofDays = len(ApiData)
 ApiData = ApiData[NumberofDays - 150:NumberofDays]
 ApiDataFrame = pandas.DataFrame(ApiData)
 
-#TOP SKILL #3: DECLARING VARIABLES (EMPTY BOX)
+#TOP SKILL 3: DECLARING VARIABLES (EMPTY BOX)
 WLPRColor = []
 WLPRPoints = []
 
-#TOP SKILL #2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
+#TOP SKILL 2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
 for i in ApiData:
     if i['wlpr'] > -50:
      iWLPRColor = 'green'
