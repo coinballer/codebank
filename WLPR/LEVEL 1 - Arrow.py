3470270fef6394e103850f7fe3c3a3af7df23e22
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-19 13:03:30 -0600

    Update LEVEL 1 - Arrow.py

--- WLPR/LEVEL 1 - Arrow.py
+++ WLPR/LEVEL 1 - Arrow.py
@@ -1,9 +1,9 @@
-#TOP SKILL #1: IMPORT PYTHON LIBRARIES (PYTHON CODING)
+#TOP SKILL 1: IMPORT PYTHON LIBRARIES (PYTHON CODING)
 import requests
 from IPython.display import Image, display
 
-#TOP SKILL #2: GET API DATA
-getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+#TOP SKILL 2: GET API DATA
+getApiData = requests.get("https://coinballer.io/api/pro-daily/APIKEY")
 ApiData = getApiData.json()
 
 #TOP SKILL 3: DECLARE AND SET VARIABLES
@@ -11,7 +11,7 @@ ApiData = getApiData.json()
 ApiData_Today = ApiData[-1]
 WLPR = ApiData_Today['wlpr']
 
-#TOP SKILL #4: SIMPLE IF STATEMENT
+#TOP SKILL 4: SIMPLE IF STATEMENT
 if WLPR > -50:
     Arrow = 'https://i.imgur.com/aCTsoA0.png'
 else: Arrow = 'https://i.imgur.com/8Li7d4A.png'
