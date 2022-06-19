a7d8c4de8d5b229340ade34970b32ed70aa38522
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-19 13:05:45 -0600

    Update LEVEL 5 - Arrow, Gauge & Charts.py

--- WLPR/LEVEL 5 - Arrow, Gauge & Charts.py
+++ WLPR/LEVEL 5 - Arrow, Gauge & Charts.py
@@ -1,12 +1,12 @@
-#TOP SKILL #1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
+#TOP SKILL 1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
 !pip install plotly
 import requests
 import plotly.express
 import pandas
 from IPython.display import Image, display
 
-#TOP SKILL #2: IMPORTING API DATA
-getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+#TOP SKILL 2: IMPORTING API DATA
+getApiData = requests.get("https://coinballer.io/api/pro-daily/APIKEY")
 ApiData = getApiData.json()
 
 #TOP SKILL 3: DECLARE AND SET VARIABLES
@@ -15,7 +15,7 @@ ApiData_Today = ApiData[-1]
 WLPR = ApiData_Today['wlpr']
 WLPRPoints = 100 + ApiData_Today['wlpr']
 
-#TOP SKILL #4: SIMPLE IF STATEMENT
+#TOP SKILL 4: SIMPLE IF STATEMENT
 if WLPR > -50:
     Arrow = 'https://i.imgur.com/aCTsoA0.png'
 else: Arrow = 'https://i.imgur.com/8Li7d4A.png'
@@ -34,7 +34,7 @@ Gauge.update_traces(marker_color=GaugeColor)
 Gauge.update_layout(yaxis_range=[0,100], plot_bgcolor='white', width=300, height=500)
 Gauge.update_xaxes(visible=True,showticklabels=False)
   
-#TOP SKILL #3: DECLARE AND SET VARIABLES
+#TOP SKILL 3: DECLARE AND SET VARIABLES
 #Most Recent 150 Days (Slice Data)
 NumberofDays = len(ApiData)
 ApiData = ApiData[NumberofDays - 150:NumberofDays]
@@ -44,11 +44,11 @@ MaxPrice = max(ApiDataFrame.price_close)
 MinDate = min(ApiDataFrame.date)
 MaxDate = max(ApiDataFrame.date) 
 
-#TOP SKILL #3: DECLARING VARIABLES (EMPTY BOX)
+#TOP SKILL 3: DECLARING VARIABLES (EMPTY BOX)
 WLPRColor = []
 WLPRPoints = []
 
-#TOP SKILL #2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
+#TOP SKILL 2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
 for i in ApiData:
     if i['wlpr'] > -50:
      iWLPRColor = 'green'
