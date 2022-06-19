162424e4faf81551de00fc327f222d39056b550e
coinballer <104280192+coinballer@users.noreply.github.com>
2022-06-19 13:03:03 -0600

    Update LEVEL 5 - Arrow, Gauge, & Charts.py

--- RSI/LEVEL 5 - Arrow, Gauge, & Charts.py
+++ RSI/LEVEL 5 - Arrow, Gauge, & Charts.py
@@ -1,12 +1,12 @@
-#TOP SKILL #1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
+#TOP SKILL 1: PYTHON CODING - IMPORT AND USE PYTHON LIBRARIES
 !pip install plotly
 import requests
 import plotly.express
 import pandas
 from IPython.display import Image, display
 
-#TOP SKILL #2: IMPORTING API DATA
-getApiData = requests.get("https://coinballer.io/api/technical-indicators-daily/APIKEY")
+#TOP SKILL 2: IMPORTING API DATA
+getApiData = requests.get("https://coinballer.io/api/pro-daily/APIKEY")
 ApiData = getApiData.json()
 
 #TOP SKILL 3: DECLARE AND SET VARIABLES
@@ -14,12 +14,12 @@ ApiData = getApiData.json()
 ApiData_Today = ApiData[-1]
 RSI = ApiData_Today['rsi']
 
-#TOP SKILL #4: SIMPLE IF STATEMENT
+#TOP SKILL 4: SIMPLE IF STATEMENT
 if RSI > 50:
     Arrow = 'https://i.imgur.com/aCTsoA0.png'
 else: Arrow = 'https://i.imgur.com/8Li7d4A.png'
   
-#TOP SKILL #4: SIMPLE IF STATEMENT
+#TOP SKILL 4: SIMPLE IF STATEMENT
 if RSI > 50:
     GaugeColor = 'green'
 else: GaugeColor = 'red'
@@ -33,16 +33,16 @@ Gauge.update_traces(marker_color=GaugeColor)
 Gauge.update_layout(yaxis_range=[0,100], plot_bgcolor='white', width=300, height=500)
 Gauge.update_xaxes(visible=True,showticklabels=False)
 
-#TOP SKILL #3: DECLARE AND SET VARIABLES
+#TOP SKILL 3: DECLARE AND SET VARIABLES
 #Most Recent 150 Days (Slice Data)
 NumberofDays = len(ApiData)
 ApiData = ApiData[NumberofDays - 150:NumberofDays]
 ApiDataFrame = pandas.DataFrame(ApiData)
 
-#TOP SKILL #3: DECLARING VARIABLES (EMPTY BOX)
+#TOP SKILL 3: DECLARING VARIABLES (EMPTY BOX)
 RSIColor = []
 
-#TOP SKILL #2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
+#TOP SKILL 2: SETTING VARIABLES WITH A BASIC LOOP (FILL THE EMPTY BOX)
 for i in ApiData:
     if i['rsi'] > 50:
      iRSIColor = 'green'
